79c6b0ad6e09521ac0f64a9c648961ec2cb0ea8b
Bruno Portilho <brunovrportilho@gmail.com>
2024-09-06 00:32:16 +0000

    add integration tests

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -32,8 +32,8 @@ export class ProductsService {
 		const product = await this.findOne(id);
 		await product.destroy();
 	}
-	async update(id: string, updateProductDto: UpdateProductDto):
-		Promise<Product> {
+	async update(id: string, updateProductDto: UpdateProductDto,
+	     ): Promise<Product> {
 		const product = await this.findOne(id);
 		const productUpdated = {
 			name: updateProductDto.name,
